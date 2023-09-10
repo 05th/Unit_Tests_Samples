9c73e0e33ab858daaad7d6f0dc7e5cbac244f040
Russ <qatsby@yandex.com>
2023-09-09 23:16:14 -0400

    class AssertEqual added

---
+++ src/test/java/AssertEquals.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AssertEquals {
+
+    //    Compare integers
+    @Test
+    public void shouldBeTwoNumbersEqual() {
+        int actual = 5;
+        int expected = 5;
+        assertEquals(actual, expected);
+    }
+
+    //    Compare Double
+    @Test
+    public void shouldBeTwoDoubleEquals() {
+        double actual = 150.00;
+        double expected = 150.04;
+        double delta = 0.05; // Delta is max difference between actual and expected result
+        assertEquals(actual, expected, delta);
+    }
+
+    //    Compare Strings
+    @Test
+    public void shouldBeTwoStringEqual() {
+        String actual = "Hello World in Java";
+        String expected = "Hello World in Java";
+        assertEquals("This message will be shown, if test failed", actual, expected);
+    }
+}
