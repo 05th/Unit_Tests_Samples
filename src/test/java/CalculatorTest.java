9cffc65739618a6dfc60db7d26072c1b016cf838
Russ <qatsby@yandex.com>
2023-09-10 21:23:20 -0400

    class update

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,22 +1,25 @@
+import classes.Calculator;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
 
-// Test sum positive numbers
+    Calculator calculator = new Calculator(); // create new object
+
+    // Test sum positive numbers
     @Test
     public void shouldBeSumPositive() {
-        Calculator calculator = new Calculator(); // create new object
         int firstNumber = 1;
         int secondNumber = 9;
         int actualResult = calculator.sum(firstNumber, secondNumber); // called the method being tested
         int expectedResult = 10; // actual result
         assertEquals(actualResult, expectedResult); // compare actual && expected results
     }
-//    Test sum positive number && zero
+
+    //    Test sum positive number && zero
     @Test
     public void shouldBeSumPositiveAndZero() {
-        Calculator calculator = new Calculator();
         int firstNumber = 1;
         int secondNumber = 0;
         int actualResult = calculator.sum(firstNumber, secondNumber);
